42a75ee037f406fdc5f916c387c0fabb83ba30ba
someone <67842643+erstert3st@users.noreply.github.com>
2025-08-23 23:18:58 +0200

    testing

--- dotfiles/config/zsh/style.sh
+++ dotfiles/config/zsh/style.sh
@@ -1,6 +1,11 @@
 #ZSH_THEME="bira"
-current_shell="$(basename "$SHELL")"
-eval "$(oh-my-posh init "$current_shell" --config "$HOME/dotfiles/emodipt-extend.omp.json")"
+# not working none interactive
+# current_shell="$(basename "$SHELL")"
+# if [[ "$SHELL" == *"bash"* ]]; then
+#   eval "$(oh-my-posh init bash --config "$HOME/dotfiles/emodipt-extend.omp.json")"
+# else
+  eval "$(oh-my-posh init  --config "$HOME/dotfiles/emodipt-extend.omp.json")"
+# fi
 # Check if both pokemon-colorscripts and fastfetch are installed
 if hash pokemon-colorscripts 2>/dev/null && hash fastfetch 2>/dev/null; then
   RANDOM=$(date +%N)
