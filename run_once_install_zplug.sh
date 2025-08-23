322fee8620d8e8d80c168424059711acab55f9bf
someone <67842643+erstert3st@users.noreply.github.com>
2025-08-23 23:05:03 +0200

    fix ?

--- run_once_install_zplug.sh
+++ run_once_install_zplug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.zplug" ]; then
+if [ ! -d "$ZPLUG_HOME" ] || [ -z "$(ls -A "$ZPLUG_HOME" 2>/dev/null)" ]; then
 	export ZPLUG_HOME=/$HOME/.zplug
 	ZPLUG_HOME=$HOME/.zplug
 	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
