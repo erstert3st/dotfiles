4fd6252f33be3fbf7e93410b4492472ed62f5c38
someone <67842643+erstert3st@users.noreply.github.com>
2025-08-23 23:10:15 +0200

    -.-""

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 #Todo get rid off oh my zsh -> 
+
+if [ ! -d "$ZPLUG_HOME" ] || [ -z "$(ls -A "$ZPLUG_HOME" 2>/dev/null)" ]; then
+	export ZPLUG_HOME=/$HOME/.zplug
+	ZPLUG_HOME=$HOME/.zplug
+	rm -rf "$ZPLUG_HOME"
+	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+	echo "Zplug is now installed."
+else
+	echo "Zplug is available."
+fi
+
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
