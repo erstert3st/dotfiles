877936b8fb376cff8b7c9b9d3049d5bd57683bb5
someone <67842643+erstert3st@users.noreply.github.com>
2025-08-30 15:11:42 +0200

    zc

--- dotfiles/config/zsh/alias.sh
+++ dotfiles/config/zsh/alias.sh
@@ -3,20 +3,15 @@ source ~/dotfiles/config/zsh/style.sh
 
 # Advanced command-not-found hook
 if [[ -f /usr/share/doc/find-the-command/ftc.bash ]]; then
-  source /usr/share/doc/find-the-command/ftc.bash
+    source /usr/share/doc/find-the-command/ftc.bash
 fi
 
 
-alias ls='eza --icons=auto'
+
 alias kubectl=kubecolor
 alias upgrade="sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt clean"
 alias python='python3'
 alias k9ss='/usr/bin/k9s'
-alias l='ls -l'
-alias la='ls -a'
-alias lla='ls -la'
-alias lll='ls -la'
-alias lt='ls --tree'
 
 ##arch
 
@@ -28,7 +23,7 @@ alias cu='chezmoi update'
 alias caa='chezmoi apply && source ~/.zshrc && echo dotfiles updates applied'
 alias caat='chezmoi apply && source ~/.zshrc && tmux source-file ~/.tmux.conf && echo dotfiles updates applied'
 alias codew="'/mnt/c/Users/user-win/AppData/Local/Programs/Microsoft VS Code/bin/code'"
-alias ccode="chezmoi cd && codew" 
+alias ccode="chezmoi cd && codew"
 # eval $(thefuck --alias)
 # eval $(thefuck --alias fuck)
 # eval $(thefuck --alias f)
@@ -40,23 +35,29 @@ alias k9s="export SHOW_KUBE_CONTEXT=true; k9s"
 
 # Replace ls with eza
 if [[ -x /usr/bin/eza ]]; then
-  alias ls='eza -al --color=always --group-directories-first --icons'     # preferred listing
-  alias la='eza -a --color=always --group-directories-first --icons'      # all files and dirs
-  alias ll='eza -l --color=always --group-directories-first --icons'      # long format
-  alias lt='eza -aT --color=always --group-directories-first --icons'     # tree listing
-  alias l.='eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+    alias ls='eza -al --color=always --group-directories-first --icons'     # preferred listing
+    alias la='eza -a --color=always --group-directories-first --icons'      # all files and dirs
+    alias ll='eza -l --color=always --group-directories-first --icons'      # long format
+    alias lt='eza -aT --color=always --group-directories-first --icons'     # tree listing
+    alias l.='eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+else
+    alias l='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lll='ls -la'
+    alias lt='ls --tree'
 fi
 
 
 # Replace some more things with better alternatives
 if [[ -x /usr/bin/bat ]]; then
-  alias cat='bat --style header --style snip --style changes --style header'
+    alias cat='bat --style header --style snip --style changes --style header'
 fi
 
 [ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
 if [[ -f /etc/arch-release ]] || grep -q "Arch Linux" /etc/os-release 2>/dev/null; then
     is_arch=true
-
+    
     # export NO_TMUX=1
     # Common use
     alias fixpacman="sudo rm /var/lib/pacman/db.lck"
--- dotfiles/config/zsh/style.sh
+++ dotfiles/config/zsh/style.sh
@@ -1,11 +1,11 @@
 #ZSH_THEME="bira"
 # not working none interactive
 # current_shell="$(basename "$SHELL")"
-# if [[ "$SHELL" == *"bash"* ]]; then
-#   eval "$(oh-my-posh init bash --config "$HOME/dotfiles/emodipt-extend.omp.json")"
-# else
+if [[ "$current_shell" == "bash" ]]; then
+  eval "$(oh-my-posh init bash --config "$HOME/dotfiles/emodipt-extend.omp.json")"
+else
   eval "$(oh-my-posh init zsh --config "$HOME/dotfiles/emodipt-extend.omp.json")"
-# fi
+fi
 # Check if both pokemon-colorscripts and fastfetch are installed
 if hash pokemon-colorscripts 2>/dev/null && hash fastfetch 2>/dev/null; then
   RANDOM=$(date +%N)
